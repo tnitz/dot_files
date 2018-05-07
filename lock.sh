e3dca9e80981537d5d919281d60ba631d6ba4f95
Ted Nitz <ted.nitz@getcruise.com>
2018-05-07 11:29:27 -0700

    Add my lock script, and some handy shortcuts.

---
+++ lock.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# i3lock blurred screen inspired by /u/patopop007 and the blog post
+# http://plankenau.com/blog/post-10/gaussianlock
+
+# Timings are on an Intel i7-2630QM @ 2.00GHz
+
+# Dependencies:
+# imagemagick
+# i3lock
+# scrot (optional but default)
+
+IMAGE=/tmp/i3lock.png
+SCREENSHOT="scrot $IMAGE" # 0.46s
+
+# Get the screenshot, add the blur and lock the screen with it
+$SCREENSHOT
+mogrify -resize 25% -blur 0x2 -spread 4 -resize 400% $IMAGE
+i3lock -i $IMAGE
+rm $IMAGE
+sleep 300; pgrep i3lock && xset dpms force off
